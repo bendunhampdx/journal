bca1c86bf46a1fa8f892187f6697fa57230ba6b8
bendunhampdx <bendunhampdx@gmail.com>
2021-08-30 15:09:20 -0700

    get form to submit and show details
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-// import Entry from './journal.js';
+// import {Journal} from './journal.js';
+// import { Entry } from './journal.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+// let yourJournal = new Journal();
+
 $('#makeEntry').click(function() {
   $('#newEntry').show();
   $('#makeEntry').hide();
 });
+
+$('form#journal').submit(function(event) {
+  event.preventDefault();
+  const inputtedDate = $('input#date').val();
+  const inputtedTitle = $('input#title').val();
+  console.log(inputtedTitle);
+  const inputtedText = $('input#text-area').val();
+  $("#date1").text(inputtedDate);
+  $("#title1").text(inputtedTitle);
+  $("#entry1").text(inputtedText);
+});
+
+// function displayEntryTitles(entryToDisplay) {
+//   let entryList = $("ul#entries");
+//   let htmlForJournalEntry = "";
+//   Object.keys(entryToDisplay.entries).forEach(function(key) {
+//     const entry = entryToDisplay.findEntry(key);
+//     htmlForJournalEntry += "<li id=" + entry.id + ">" + entry.title + " " + entry.date + "</li>";
+//   });
+//   journalEntries.html(htmlForJournalEntry);
+// }
+ 
--- src/journal.js
+++ src/journal.js
@@ -1,8 +1,35 @@
-export default function Entry(title, body) {
-  this.title = title;
-  this.body = body;
+// Business logic for Journal
+
+export function Journal() {
+  this.entries = {};
+  this.entryId = 0;
 }
 
-Entry.prototype.wordCount = function() {
-  return "I can't do this yet"
-}
+Journal.prototype.addEntry = function(entry) {
+  entry.id = this.assignId();
+  this.entries[entry.id] = entry;
+};
+
+Journal.prototype.assignId = function() {
+  this.entryId += 1;
+  return this.entryId;
+};
+
+// Journal.prototype.findEntry = function(id) {
+//   if (this.entries[id] != undefined) {
+//     return this.entries[id];
+//   }
+//   return false;
+// };
+
+// Business logic for Entry
+
+// export function Entry(date, title, text) {
+//   this.date = date;
+//   this.title = title;
+//   this.text = text;
+// }
+
+// Entry.prototype.getTeaser = function() {
+//   const teaser = (inputtedText.split(" ", 8)).join();
+// };
